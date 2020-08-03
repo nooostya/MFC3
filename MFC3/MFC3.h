50fbaf623f0821722d110556f38a081ab8d5d867
Anastasia Lozytska <trall088@gmail.com>
2020-08-03 12:35:13 +0300

    Add project files.

---
+++ MFC3/MFC3.h
@@ -0,0 +1,32 @@
+﻿
+// MFC3.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFC3App:
+// See MFC3.cpp for the implementation of this class
+//
+
+class CMFC3App : public CWinApp
+{
+public:
+	CMFC3App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC3App theApp;
